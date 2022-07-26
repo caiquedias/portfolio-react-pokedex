530dea900b5678ff1c901230b1b1765d93e485b3
caique.araujo <caique.araujo@dorconsultoria.com.br>
2022-07-26 12:14:05 -0300

    ajuste para obtenção de descrição em caso de retorno nulo em objeto de caracteristicas

--- src/components/App.js
+++ src/components/App.js
@@ -1,67 +1,80 @@
-import React, { Component } from 'react';
-import PokeList from './PokeList';
-import DetailView from './DetailView';
-import './styles/App.css';
-import Pokemon from '../Pokemon';
-import PokemonCharacteristic from '../PokemonCharacteristic';
+import React, { Component } from "react";
+import PokeList from "./PokeList";
+import DetailView from "./DetailView";
+import "./styles/App.css";
+import Pokemon from "../Pokemon";
+import PokemonCharacteristic from "../PokemonCharacteristic";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       pokemon: {},
-      pokemonCharacteristic: {}
+      pokemonCharacteristic: {},
     };
 
     this.handleOnClick = this.handleOnClick.bind(this);
   }
 
-  handleOnClick(id){
+  handleOnClick(id) {
     fetch(`http://pokeapi.co/api/v2/pokemon/${id}/`)
-    .then(res => res.json())
-    .then(data => {
-      const pokemon = new Pokemon(data);
+      .then((res) => res.json())
+      .then((data) => {
+        const pokemon = new Pokemon(data);
 
-      this.setState({ pokemon });
+        this.setState({ pokemon });
 
-      this.getCharacteristics(id);
-    })
-    .catch(err => alert(err));
+        this.getCharacteristics(id);
+      })
+      .catch((err) => alert(err));
   }
 
-  getCharacteristics(id){
+  getCharacteristics(id) {
     fetch(`https://pokeapi.co/api/v2/characteristic/${id}/`)
-    .then(res => res.json())
-    .then(data => {
+      .then((res) => res.json())
+      .then((data) => {
+        let characteristic = null;
+        let pokemonCharacteristic = null;
+        const descriptionList = data.descriptions;
 
-      let characteristic = null;
-      let pokemonCharacteristic = null;
-      const descriptionList = data.descriptions;
-      
-      if(descriptionList.length > 0 || data === 'Not Found'){
-          characteristic = { 
-          description: data.descriptions.filter(element => element.language.name === 'en')[0].description
-        };
-     }else{
-          characteristic = { 
-          description: 'No description available'
-        };
-     }
+        if (descriptionList.length > 0 || data === "Not Found") {
+          characteristic = {
+            description: data.descriptions.filter(
+              (element) => element.language.name === "en"
+            )[0].description,
+          };
+        } else {
+          characteristic = {
+            description: "No description available",
+          };
+        }
 
         pokemonCharacteristic = new PokemonCharacteristic(characteristic);
         this.setState({ pokemonCharacteristic });
-
-    })
-    .catch(err => alert(err));
+      })
+      .catch((err) => this.getDefaultDescription());
   }
 
-  render(){
+  getDefaultDescription() {
+    const characteristic = {
+      description: "No description available",
+    };
+
+    const 
+    pokemonCharacteristic = new PokemonCharacteristic(characteristic);
+    this.setState({ pokemonCharacteristic });
+  }
+
+  render() {
     return (
       <div className="App">
-        <PokeList handleOnClick={this.handleOnClick}/>
-        <DetailView pokemon={ this.state.pokemon } pokemonCharacteristic={this.state.pokemonCharacteristic}/>
+        <PokeList handleOnClick={this.handleOnClick} />
+        <DetailView
+          pokemon={this.state.pokemon}
+          pokemonCharacteristic={this.state.pokemonCharacteristic}
+        />
       </div>
-    )
+    );
   }
 }
 
